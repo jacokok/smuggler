899bc5c5f67130cb743495d6bfc8554a44b985b2
Jaco Kok <kokjaco2@gmail.com>
2021-12-11 12:51:38 +0200

    Download file now

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM zenika/alpine-chrome:with-playwright
 USER root
 ENV NODE_ENV=production
-EXPOSE 3000
+ENV PORT=3000
+ENV HOST="0.0.0.0"
+EXPOSE ${PORT}
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 const express = require("express");
 const { chromium } = require("playwright-chromium");
 
-// Constants
-const PORT = 3000;
-const HOST = "0.0.0.0";
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 
-// App
+var defaultOptions = {
+  scale: 1,
+  displayHeaderFooter: false,
+  headerTemplate: "",
+  footerTemplate: "",
+  printBackground: false,
+  landscape: false,
+  pageRanges: "",
+  format: "A4",
+  width: "",
+  height: "",
+  margin: {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  preferCSSPageSize: false,
+  omitBackground: false,
+  timeout: 30,
+};
+
 const app = express();
 app.get("/", (req, res) => {
-  (async () => {
-    const browser = await chromium.launch({
-      executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
-      // 💡 This enables logs for the communication between Playwright and Chromium
-      // logger: {
-      //   isEnabled: (name, severity) => name === "browser" || "context",
-      //   log: (name, severity, message, args) => console.log(`${name} ${message}`),
-      // },
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("http://whatsmyuseragent.org/");
-    await page.screenshot({ path: `src/example-chromium.png` });
-    await browser.close();
-  })();
-  res.send("Hello World");
+  // res.send("Information and stuff");
+  res.send(defaultOptions);
+});
+
+app.get("/url", async (req, res) => {
+  const browser = await chromium.launch({
+    executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://whatsmyuseragent.org/");
+  // const result = await page.pdf();
+  var result = await page.screenshot();
+  await browser.close();
+  res.end(Buffer.from(result, "binary"));
+});
+
+app.post("/url", async (req, res) => {
+  const browser = await chromium.launch({
+    executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://whatsmyuseragent.org/");
+  const result = await page.pdf();
+  await browser.close();
+  res.end(Buffer.from(result, "binary"));
 });
 
 app.listen(PORT, HOST);
